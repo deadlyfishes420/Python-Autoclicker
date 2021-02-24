265d8ba2c614bfe046e219c3737eec36ec1e8944
deadlyfishes420 <richardhovsepyan12@gmail.com>
2021-02-24 14:44:19 +0000

    updated delay

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
 
-delay = input("set delay: ")
+delay = int(input("set delay: "))
 button = Button.left
 start_stop_key = KeyCode(char='s')
 exit_key = KeyCode(char='e')
